dd806edea322b59954dab43f480e677f273e3df5
SS-okamura <ss.okamura0509@gmail.com>
2024-02-22 13:12:34 +0900

    scaffold Chick

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_041145) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_041217) do
   create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chicks", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
