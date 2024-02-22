6392c1635e6f2440274bb2c3d3453dd9e1e11ef8
SS-okamura <ss.okamura0509@gmail.com>
2024-02-22 13:12:07 +0900

    scaffold Dog

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_034639) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_22_041145) do
   create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
